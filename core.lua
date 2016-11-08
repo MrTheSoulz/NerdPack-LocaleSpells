6514db1b4c2e4a7de0721f912f3f292bf3fe0dc3
Jaime Marques <mrthesoulz@gmail.com>
2016-11-08 15:01:53 +0000

    update spells

--- core.lua
+++ core.lua
@@ -1,5 +1,12 @@
-local _, gbl = ...
-local NeP    = NeP
+local _, gbl    = ...
+local NeP       = NeP
+local UnitClass = UnitClass
 
-NeP.Spells:Add(gbl.SpellID)
-NeP.Spells:Add(gbl.Talents)
+NeP.Listener:Add('NeP_Spells', 'PLAYER_LOGIN', function()
+  local classIndex = select(3, UnitClass('player'))
+  NeP.Spells:Add(gbl.Class[classIndex])
+  -- After 15 sec, removed everything localy
+  C_Timer.After(15, function()
+    gbl.Class = nil
+  end)
+end)
