6325445790d3fc04d3a331153df7f4df5c9572c8
Jaime Marques <mrthesoulz@gmail.com>
2016-11-13 17:07:18 +0000

    replace listener with Core:WhenInGame

--- core.lua
+++ core.lua
@@ -1,12 +1,13 @@
 local _, gbl    = ...
 local NeP       = NeP
 local UnitClass = UnitClass
+local C_Timer   = C_Timer
 
-NeP.Listener:Add('NeP_Spells', 'PLAYER_LOGIN', function()
+NeP.Core:WhenInGame(function()
   local classIndex = select(3, UnitClass('player'))
   NeP.Spells:Add(gbl.Class[classIndex])
   -- After 15 sec, removed everything localy
   C_Timer.After(15, function()
     gbl.Class = nil
   end)
-end)
+end, 0)
